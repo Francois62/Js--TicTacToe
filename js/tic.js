635d90e2d34ab12e301614f80636058032ced93c
Francois62 <franois.andrzejewski@laposte.net>
2016-11-29 12:30:42 +0100

    presque

---
+++ js/tic.js
@@ -0,0 +1,99 @@
+document.getElementById('tour').innerHTML="Au Joueur 1 de commencer";
+var table = document.getElementById('tableau');
+var cell = table.getElementsByTagName('td');
+  for (var i=0; i<9; i++){
+  cell[i].onclick = switchcol;
+}
+var t=0;
+function switchcol(e){
+  var y=1;
+  y=y+t;
+  var i = cell[i];
+      if(e.target.className!="player1" && e.target.className!="player2"){
+  if(t==1 || t==3 || t==5 || t==7 ){
+        e.target.className="player2";
+        document.getElementById('tour').innerHTML="Au Joueur 1";
+        console.log(y + " " + "tour");
+        console.log(cell[i] + "joueur 2 à joué");
+      }
+      else {
+          e.target.className="player1";
+          document.getElementById('tour').innerHTML="Au Joueur 2";
+          console.log(y + " "  +"tour");
+          console.log(cell[i] + "joueur 1 à joué");
+      }
+      i++;
+      t++;
+      }
+      victory();
+}
+  var win = document.getElementsByTagName('div')[0];
+function victory(){
+if(cell[0].className=="player1" && cell[1].className=="player1" && cell[2].className=="player1"){
+  //var win = document.getElementsByTagName('div')
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[3].className=="player1" && cell[4].className=="player1" && cell[5].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[6].className=="player1" && cell[7].className=="player1" && cell[8].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[0].className=="player1" && cell[3].className=="player1" && cell[6].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[1].className=="player1" && cell[4].className=="player1" && cell[7].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[2].className=="player1" && cell[5].className=="player1" && cell[8].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[0].className=="player1" && cell[4].className=="player1" && cell[8].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+else if(cell[2].className=="player1" && cell[4].className=="player1" && cell[6].className=="player1"){
+  win.className="winner1";
+  win.innerHTML="Joueur 1 à gagner";
+}
+
+
+else if(cell[0].className=="player2" && cell[1].className=="player2" && cell[2].className=="player2"){
+  win.className="winner2";
+  win.innerHTML="Joueur 2 à gagner";
+}
+  else if(cell[3].className=="player2" && cell[4].className=="player2" && cell[5].className=="player2"){
+    win.className="winner2";
+    win.innerHTML="Joueur 2 à gagner";
+  }
+  else if(cell[6].className=="player2" && cell[7].className=="player2" && cell[8].className=="player2"){
+    win.className="winner2";
+    win.innerHTML="Joueur 2 à gagner";
+  }
+    else if(cell[0].className=="player2" && cell[3].className=="player2" && cell[6].className=="player2"){
+      win.className="winner2";
+      win.innerHTML="Joueur 2 à gagner";
+    }
+    else if(cell[1].className=="player2" && cell[4].className=="player2" && cell[7].className=="player2"){
+      win.className="winner2";
+      win.innerHTML="Joueur 2 à gagner";
+    }
+    else if(cell[2].className=="player2" && cell[5].className=="player2" && cell[8].className=="player2"){
+      win.className="winner2";
+      win.innerHTML="Joueur 2 à gagner";
+    }
+    else if(cell[0].className=="player2" && cell[4].className=="player2" && cell[8].className=="player2"){
+      win.className="winner2";
+      win.innerHTML="Joueur 2 à gagner";
+    }
+    else if(cell[2].className=="player2" && cell[4].className=="player2" && cell[6].className=="player2"){
+      win.className="winner2";
+      win.innerHTML="Joueur 2 à gagner";
+    }
+}
